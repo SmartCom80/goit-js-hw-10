5d3501dd0c54dc7ae62049ddfd046a0382834edb
SmartCom80 <smartcom.ter@gmail.com>
2022-08-22 01:25:15 +0300

    Added task__01

---
+++ src/fetchCountries.js
@@ -0,0 +1,16 @@
+//функція відправки і отримання відповідні на запрос до бекенду сайта
+const BASE_URL = 'https://restcountries.com/v3.1/name';
+
+function fetchCountries(name) {
+  console.log('name :>> ', name);
+  return fetch(
+    `${BASE_URL}/${name}?fields=name,capital,population,flags,languages`
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
+
+export default { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,58 @@
+import templCountryListMarkup from './templates/templCountryList.hbs';
+import templCountryInfoMarkup from './templates/templCountryInfo.hbs';
+import debounce from 'lodash.debounce';
+import API from '../src/fetchCountries.js';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+let textSearchField = 0;
+
+const refs = {
+  inputField: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
+};
+
+// Викликаємо прослуховувач для події input в полі inputField
+refs.inputField.addEventListener(
+  'input',
+  debounce(onTextFieldInput, DEBOUNCE_DELAY)
+);
+
+// Функція отримання значення з поля вводу input
+function onTextFieldInput(event) {
+  textSearchField = event.target.value.trim();
+  checkedTextField(textSearchField);
+}
+
+//функція перевірки значення данних в полі вводу
+function checkedTextField(textSearchField) {
+  console.log('textSearchField.length :>> ', textSearchField.length);
+  if (textSearchField.length > 0) {
+    API.fetchCountries(textSearchField).then(renderMarkup).catch(onFetchError);
+  } else if (textSearchField.length === 0) {
+    refs.countryList.innerHTML = '';
+    refs.countryInfo.innerHTML = '';
+  }
+}
+
+//  Функція виконання розмітки
+function renderMarkup(fetchCountry) {
+  const sizeArray = fetchCountry.length;
+
+  if (sizeArray > 10) {
+    Notify.info('Too many matches found. Please enter a more specific name.');
+  } else if (sizeArray > 1 && sizeArray < 10) {
+    refs.countryInfo.innerHTML = '';
+    refs.countryList.innerHTML = templCountryListMarkup(fetchCountry);
+  } else if (sizeArray === 1) {
+    refs.countryList.innerHTML = '';
+    refs.countryInfo.innerHTML = templCountryInfoMarkup(fetchCountry);
+  }
+}
+
+//Функція видає попереджуюче сповіщення про помилку запиту
+function onFetchError() {
+  Notify.failure('Oops, there is no country with that name');
+}
